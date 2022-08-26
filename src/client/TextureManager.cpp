6df8dac030d65534d9010ddf27a72635547eed31
Creepii <www.creepsythecreep@gmail.com>
2022-08-26 21:37:24 +0200

    added resource folder macro

--- src/client/TextureManager.cpp
+++ src/client/TextureManager.cpp
@@ -7,13 +7,13 @@ TextureManager::TextureManager() {
 }
 
 void TextureManager::load_textures() {
-    this->load_texture("holy_salad", "../res/holy_salad.png");
-    this->load_texture("powerup", "../res/powerup.png");
-    this->load_texture("turtle", "../res/turtle.png");
-    this->load_texture("turtle_opening", "../res/turtle_hatch_opening.png");
-    this->load_texture("turtle_open", "../res/turtle_hatch_open.png");
-    this->load_texture("turtle_shooting", "../res/turtle_hatch_shooting.png");
-    this->load_texture("logo", "../res/logo.png");
+    this->load_texture("holy_salad", RESOURCE_FOLDER"/holy_salad.png");
+    this->load_texture("powerup", RESOURCE_FOLDER"/powerup.png");
+    this->load_texture("turtle", RESOURCE_FOLDER"/turtle.png");
+    this->load_texture("turtle_opening", RESOURCE_FOLDER"/turtle_hatch_opening.png");
+    this->load_texture("turtle_open", RESOURCE_FOLDER"/turtle_hatch_open.png");
+    this->load_texture("turtle_shooting", RESOURCE_FOLDER"/turtle_hatch_shooting.png");
+    this->load_texture("logo", RESOURCE_FOLDER"/logo.png");
 }
 
 void TextureManager::load_texture(std::string name, std::string filename) {
